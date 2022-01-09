31295a70576919032df761f71de1ec522c465ef0
Matheus Araujo <matheus.a@havan.com.br>
2022-01-09 12:10:24 -0300

    Componente: TranscationsTable

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from "../Summary";
+import { TranscactionsTable } from "../TransactionsTable";
 import { Container } from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
       <Summary />
+      <TranscactionsTable />
     </Container>
   );
 }
