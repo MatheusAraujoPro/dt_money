c3590ba217576fcec574b960759e105a3f910f49
Matheus Araujo <matheus.a@havan.com.br>
2022-01-09 10:44:52 -0300

    Fontes do Google Fonts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -37,6 +37,16 @@ export const GlobalStyle = createGlobalStyle`
     -webkit-font-smoothing: antialiased; 
   }
 
+  /* Por padrão o Button, Input e Text Área não importam a fonte do Body */
+  body, input, textarea, button{
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong{
+    font-weight: 600;
+  }
+
   button{
     cursor: pointer;
   }
