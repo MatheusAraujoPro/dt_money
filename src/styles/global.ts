60a40397bff2d92ec87e7a653b40ef97cb829afb
Matheus Araujo <matheus.a@havan.com.br>
2022-01-14 14:12:47 -0300

    Estilizanod modal

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -74,11 +74,25 @@ export const GlobalStyle = createGlobalStyle`
 
   .react-modal-content{
     width: 100%;
+    /* Vai ter Width 100% até o tamanho máximo especificado */
     max-width: 576px;
     background: var(--background);
     padding: 3rem;
     position: relative;
     border-radius: 0.25rem;
+  }
+
+  .react-modal-close{
+    position: absolute;
+    right: 1.5rem;
+    top: 1.5rem;
+    border: 0;
+    background: transparent;
+    transition: 0.2s;
+    
+    &:hover{
+      filter: brightness(0.9);
+    }
   }
 
 `
